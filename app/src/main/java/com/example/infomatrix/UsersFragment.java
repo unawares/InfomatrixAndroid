fcab1f50603736f7ebcb8c2b443d4238bbb98e8f
zhantore <zhantore_15@mail.ru>
2019-04-07 21:00:07 +0600

    Some changes was made

--- app/src/main/java/com/example/infomatrix/UsersFragment.java
+++ app/src/main/java/com/example/infomatrix/UsersFragment.java
@@ -1,19 +1,36 @@
 package com.example.infomatrix;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.infomatrix.fragments.StudentsHistoryFragment;
+import com.example.infomatrix.fragments.SupervisorHistoryFragment;
+
+@SuppressLint("ValidFragment")
 public class UsersFragment extends Fragment {
+
+    Context context;
+
+    @SuppressLint("ValidFragment")
+    public UsersFragment(Context context) {
+        this.context = context;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        View v = inflater.inflate(R.layout.fragment_users, container, false);
+        return v;
     }
-
 }
